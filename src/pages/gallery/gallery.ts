534c9a89d17359353788218903cf88172b1ddca7
jespert88 <jesper_truedsson@hotmail,com>
2017-10-05 09:37:15 +0200

    added all files v1

---
+++ src/pages/gallery/gallery.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'gallery.html'
+})
+export class GalleryPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { GalleryPage } from '../gallery/gallery';
+import { TourPage} from '../tour/tour';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  showGallery(){
+    this.navCtrl.push(GalleryPage);
+  }
+
+  showTour(){
+    this.navCtrl.push(TourPage);
+  }
+
+
+
+}
