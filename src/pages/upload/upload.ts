527419cbfadb9255a1bed49aec7a50104e727031
nile16 <nils.leandersson@gmail.com>
2017-10-06 12:59:19 +0200

    gjort uppladdningsidan funktionell

--- src/pages/upload/upload.ts
+++ src/pages/upload/upload.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SkateProvider } from '../../providers/skate/skate';
 
-/**
- * Generated class for the UploadPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+declare var picture;
+declare var fileInput;
+
 
 @IonicPage()
 @Component({
@@ -15,11 +13,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UploadPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  file: any;
+  nick:string = "";
+  description:string = "";
+
+  tags = [];
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public skate:SkateProvider) {
+    //this.skate.getMedia(function(media){console.log(media)});
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UploadPage');
+
+  ionViewDidLoad(){
+
+    // Get tags from skate database and create a checkbox tag list
+    this.skate.getTags((tags)=>{
+         for (var i=0;i<tags.length;i++)
+  		     this.tags.push({Name:tags[i],Value:false});
+  	   });
+
+  // Create listener that display the selected picture when a picture is selected.
+  fileInput.addEventListener("change",(e) => {
+    var fileReaderImg = new FileReader();
+  	this.file = e.target.files[0];
+  	fileReaderImg.onload = function (readerEvent) {
+  		picture.src = this.result;
+  	}
+  	fileReaderImg.readAsDataURL(fileInput.files[0]);
+  });
+
+
+}
+
+// This function is executed when the upload button is pressed
+uploadFile(){
+
+  var meta = {
+    tags:[],
+    nick:"",
+    description:""
+  };
+
+  // Check which tags are selected and add those to meta in the form of an array
+  for (var i=0;i<this.tags.length;i++){
+    if (this.tags[i].Value) meta.tags.push(this.tags[i].Name);
   }
+
+  //meta.nick = nick.value.trim();
+  meta.nick = this.nick.trim();
+
+  //meta.description = description.value.trim();
+  meta.description = this.description.trim();
+
+  this.skate.getExif(this.file,(exif) => {
+
+    Object.assign(meta,exif);
+    console.log(meta);
+    this.skate.upload(this.file,meta,function(res){
+      console.log(res);
+      alert("Uppladdning klar!")
+    });
+
+  });
+}
+
 
 }
