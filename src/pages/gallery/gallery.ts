d997fdffcdba39c1cbdabceedf67d8b13c9e6afe
jespert88 <jesper_truedsson@hotmail,com>
2017-10-06 15:14:35 +0200

    now the col system works better

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 import { SkateProvider } from '../../providers/skate/skate';
 
 
@@ -13,15 +14,19 @@ export class GalleryPage {
 
 
 
-  constructor(public navCtrl: NavController, public skate:SkateProvider) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public skate:SkateProvider) {
     this.skate.getMedia((media)=>{
       this.gallery_items = media;
       console.log(media)
     });
   }
 
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: "Vadå skateutställning ?",
+      message: "  Vad är Lorem Ipsum Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin.   Lorem ipsum har varit standard ända sedan 1500-talet",
 
-
-
-
+    });
+    prompt.present();
+  }
 }
