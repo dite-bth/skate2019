ff5ebc4020c284254a0716ea6b25051f11957938
jespert88 <jesper_truedsson@hotmail,com>
2017-10-11 10:58:17 +0200

    deleted uneccerary ts code

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -12,15 +12,6 @@ import { UploadPage } from '../upload/upload';
 export class GalleryPage {
 
   gallery_items: any;
-  shapes: string;
-  shapes = ['M0 100 L80 100 L100 80 L100 60 L80 60 L80 40 L60 40 L60 20 L40 20 L40 0 L20 0 L0 20',
-
-            'M0 100 L80 100 L100 80 L40 0 L20 0 L0 20',
-
-            'M40 100 L60 100 L60 60 L100 60 L100 40 L0 40 L0 60 L40 60 L40 100',
-
-            'M 0 100 L 80 100 L 100 80 C 100 80 30 60 40 0 L 20 0 L 0 20 L 0 100'];
-
 
 
 
@@ -45,20 +36,6 @@ export class GalleryPage {
     prompt.present();
   }
 
-
-    randomShapes() {
-     //#Paths för SVG...gjorda för viewbox(0 0 100 100)
-      let shapes = ['M0 100 L80 100 L100 80 L100 60 L80 60 L80 40 L60 40 L60 20 L40 20 L40 0 L20 0 L0 20',
-                  'M0 100 L80 100 L100 80 L40 0 L20 0 L0 20',
-                  'M40 100 L60 100 L60 60 L100 60 L100 40 L0 40 L0 60 L40 60 L40 100',
-                  'M0 100 L80 100 L100 80 C100 80 30 60 40 0 L20 0 L0 20 L0 100'];
-      let rand_shape = Math.floor((Math.random() * (3 - 0 + 1) + 0));
-      let svgTag = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 100 100" >' +
-                      '<path stroke-width="2" fill="#FF9F00"' +
-                      ' transform="rotate(0 50 50) scale(1.0)"' +
-                      ' d="' + rand_shape + '" />' +
-                      '</svg>';
-    }
 
 
 
