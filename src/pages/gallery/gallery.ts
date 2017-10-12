863e3aec7001c7ca7aceb779a142a03180c0cd4a
nile16 <nils.leandersson@gmail.com>
2017-10-12 20:40:46 +0200

    Lagt till slumpade färger till formerna.

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -17,6 +17,7 @@ export class GalleryPage {
             'M0 100 L80 100 L100 80 L40 0 L20 0 L0 20',
             'M40 100 L60 100 L60 60 L100 60 L100 40 L0 40 L0 60 L40 60 L40 100',
             'M 0 100 L 80 100 L 100 80 C 100 80 30 60 40 0 L 20 0 L 0 20 L 0 100'];
+  colors = ['#FF9F00','#FF0000','#00FF00','#0000FF'];
 
   constructor(
     public navCtrl: NavController,
@@ -30,6 +31,7 @@ export class GalleryPage {
         // Thanks to retarded angular, random shapes can not be inserted direct in HTML, but rather has to be stored first. See:
         // https://stackoverflow.com/questions/43079990/angular-2-expression-has-changed-after-it-was-checked-error-with-dynamic-image
         for (let i=0;i<this.gallery_items.length;i++) this.gallery_items[i].shape = Math.floor(Math.random() * this.shapes.length);
+        for (let i=0;i<this.gallery_items.length;i++) this.gallery_items[i].color = Math.floor(Math.random() * this.colors.length);
       });
   }
 
