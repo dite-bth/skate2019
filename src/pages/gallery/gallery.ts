ec2e534fb0331933509ca2d6b41ba229a74bcf09
jespert88 <jesper_truedsson@hotmail,com>
2017-10-12 14:20:46 +0200

    have a okey gallery now

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController, ModalController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { SkateProvider } from '../../providers/skate/skate';
 import { UploadPage } from '../upload/upload';
+
 
 
 @Component({
@@ -17,28 +18,36 @@ export class GalleryPage {
             'M40 100 L60 100 L60 60 L100 60 L100 40 L0 40 L0 60 L40 60 L40 100',
             'M 0 100 L 80 100 L 100 80 C 100 80 30 60 40 0 L 20 0 L 0 20 L 0 100'];
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public skate:SkateProvider, public modalCtrl: ModalController) {
-    this.skate.getMedia((media)=>{
-      this.gallery_items = media;
-      console.log(media);
-      // Thanks to retarded angular, random shapes can not be inserted direct in HTML, but rather has to be stored first. See:
-      // https://stackoverflow.com/questions/43079990/angular-2-expression-has-changed-after-it-was-checked-error-with-dynamic-image
-      for (let i=0;i<this.gallery_items.length;i++) this.gallery_items[i].shape = Math.floor(Math.random() * this.shapes.length);
-    });
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public skate:SkateProvider,
+    public modalCtrl: ModalController,
+    params: NavParams) {
+
+      this.skate.getMedia((media)=>{
+        this.gallery_items = media;
+
+
+        // Thanks to retarded angular, random shapes can not be inserted direct in HTML, but rather has to be stored first. See:
+        // https://stackoverflow.com/questions/43079990/angular-2-expression-has-changed-after-it-was-checked-error-with-dynamic-image
+        for (let i=0;i<this.gallery_items.length;i++) this.gallery_items[i].shape = Math.floor(Math.random() * this.shapes.length);
+      });
   }
 
   uploadPageModal() {
-  const uploadModal = this.modalCtrl.create(UploadPage);
-
-  uploadModal.present();
+    const uploadModal = this.modalCtrl.create(UploadPage);
+    uploadModal.present();
   }
 
-  showPrompt() {
-    let prompt = this.alertCtrl.create({
+  presentProfileModal(media) {
+   const profileModal = this.modalCtrl.create(GalleryPage, {media} );
+   profileModal.present();
+ }
 
-    });
-    prompt.present();
-  }
+
+
+
 
 
 }
