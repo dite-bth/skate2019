b931e2a7807e6663b951159eaf26bf35c78e2ec0
jespert88 <jesper_truedsson@hotmail,com>
2017-10-20 14:18:41 +0200

    gallery now looks better than before..

--- src/pages/modalimage/modalimage.ts
+++ src/pages/modalimage/modalimage.ts
@@ -10,10 +10,18 @@ export class ModalImagePage {
 
 url:any;
 description:any;
+nick: any;
+uploadTimeString: any;
+views: any;
+tags: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.url = this.navParams.get('url');
     this.description = this.navParams.get('description');
+    this.nick = this.navParams.get('nick');
+    this.uploadTimeString = this.navParams.get('uploadTimeString');
+    this.views = this.navParams.get('views');
+    this.tags = this.navParams.get('tags');
    }
 
 
